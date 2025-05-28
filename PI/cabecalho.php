5e53098d0198d2cac2be5576a2ef7efc1fb7a7f7
WillMoreira2129 <willianmoreira2129@gmail.com>
2025-05-28 17:03:53 -0300

    Add files via upload

---
+++ PI/cabecalho.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cabeçalho</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="cabecalho">
+    <ul class="precabecalho">
+      <li style="padding-left: 5%;">
+        <img src="./assets/Captura de tela 2025-03-14 162622.png" alt="logo-borboleta" />
+      </li>
+
+      <li style="padding-top: 3%;">
+        <input type="search" placeholder="  O que você está procurando?" aria-label="Search"
+          style="background-color: #ddccba; border-radius: 0.75rem; width: 600px; height: 60%;" />
+      </li>
+
+      <li style="padding-top: 2.9%; padding-left: auto; text-align: left;">
+        <a href="https://www.youtube.com"><img src="./assets/cadastrese.png" alt="" style="width: auto" /></a>
+      </li>
+
+      <li style="padding-top: 2.4%; padding-left: auto;">
+        <a href=""><img src="./assets/Captura de tela 2025-03-19 134529.png" alt="" style="width: 190px" /></a>
+      </li>
+    </ul>
+  </div>
+
+  <nav class="navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-brand" href="#">Navbar</span>
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Categorias <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Recomendados <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Lançamentos <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Ofertas <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Leilões</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Anuncie</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        </button>
+      </form>
+    </div>
+  </nav>
+
+
+
+  <footer>
+    <ul style="display: flex; padding-left: 42%;">
+      <li><a href="" class="infos">Contato</a></li>
+      <li><a href="" class="infos">Quem Somos</a></li>
+      <li><a href="" class="infos">Política de Devolução</a></li>
+    </ul>
+    <div style="display: flex; padding-left: 10%">
+      <ul style="display: block">
+        <li><b>Departamentos</b></li>
+        <li>Início</li>
+        <li>Lançamentos</li>
+        <li>Recomendados</li>
+        <li>Leilões</li>
+        <li>Ofertas</li>
+        <li>Anuncia</li>
+      </ul>
+
+      <ul style="display: block; padding-left: 10%">
+        <li><b>Entre em contato</b></li>
+        <li>Fone: +55 43 99999-9999</li>
+        <li>E-mail: OldBazzar51@gmail.com</li>
+        <li>Cornélio Procópio/ Cidade</li>
+      </ul>
+    </div>
+    <br />
+    <hr style="height: 1px; border-width: 0; background-color: #564031" />
+    <nav>
+      <img src="./assets/insta_ico.png" alt="" />
+      <img src="./assets/whatsapp_ico.png" alt="" />
+      <img src="./assets/avi_ico.png" alt="" />
+    </nav>
+    <p>&copy; 2025 - Todos os direitos reservados</p>
+  </footer>
+
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
